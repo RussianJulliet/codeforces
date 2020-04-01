cda4439d42efccb88ff69ee7b6852ed412cd6af5
Koniushenko Iulia <45033005+RussianJulliet@users.noreply.github.com>
2020-04-01 17:17:23 +0300

    рекурсия

---
+++ count arrays.py
@@ -0,0 +1,30 @@
+len_arr = int(input())
+array = list(map(int, input().split()))
+
+
+def min1_array(array):
+    delete = 0
+    for i in range(len(array) - 1):
+        if array[i] == array[i + 1]:
+            array[i] += 1
+            array[i + 1] = 0
+            delete += 1
+    print(array)
+    # print('*', delete)
+    if delete != 0:
+        for i in range(len(array)):
+            if array[i] == 0:
+                for k in range(i, len(array) - 1):
+                    array[k] = array[k + 1]
+        for t in range(delete):
+            array.pop()
+        print(array)
+        min1_array(array)
+    if delete == 0:
+        a = len(array)
+        return a
+
+
+print(min1_array(array))
+
+
